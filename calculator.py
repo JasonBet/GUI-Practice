a812b900bba0555321ee39eba0272fb0de4295a8
Jason B <jasonbetsargon@gmail.com>
2024-03-26 06:14:23 -0700

    completed calculator

--- calculator.py
+++ calculator.py
@@ -15,6 +15,8 @@ def button_click(number):
 def button_add():
     first_number=e.get()
     global f_num
+    global math
+    math="addition"
     f_num=int(first_number)
     e.delete(0, END)
 
@@ -24,7 +26,39 @@ def button_clear():
 def button_equal():
     second_number=e.get()
     e.delete(0, END)
-    e.insert(0, f_num+int(second_number))
+
+    if math =="addition":
+        e.insert(0, f_num + int(second_number))
+    if math =="subtraction":
+        e.insert(0, f_num - int(second_number))
+    if math =="multiplication":
+        e.insert(0, f_num * int(second_number))
+    if math =="division":
+        e.insert(0, f_num / int(second_number))
+
+def button_subtract():
+    first_number=e.get()
+    global f_num
+    global math
+    math="subtraction"
+    f_num=int(first_number)
+    e.delete(0, END)
+
+def button_multiply():
+    first_number=e.get()
+    global f_num
+    global math
+    math="multiplication"
+    f_num=int(first_number)
+    e.delete(0, END)
+
+def button_divide():
+    first_number=e.get()
+    global f_num
+    global math
+    math="division"
+    f_num=int(first_number)
+    e.delete(0, END)
 
 # Define buttons
 
@@ -39,8 +73,12 @@ button_8 = tk.Button(root, text="8", padx=40, pady=20, command=lambda: button_cl
 button_9 = tk.Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9))
 button_0 = tk.Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
 button_add = tk.Button(root, text="+", padx=39, pady=20, command=button_add)
-button_equal = tk.Button(root, text="=", padx=88, pady=20, command=button_equal)
-button_clear = tk.Button(root, text="Clear", padx=78, pady=20, command=button_clear)
+button_equal = tk.Button(root, text="=", padx=91, pady=20, command=button_equal)
+button_clear = tk.Button(root, text="Clear", padx=79, pady=20, command=button_clear)
+
+button_subtract = tk.Button(root, text="-", padx=41, pady=20, command=button_subtract)
+button_multiply = tk.Button(root, text="*", padx=40, pady=20, command=button_multiply)
+button_divide = tk.Button(root, text="/", padx=41, pady=20, command=button_divide)
 
 # Put buttons on the screen
 
@@ -61,5 +99,9 @@ button_0.grid(row=4,column=0)
 button_clear.grid(row=4,column=1,columnspan=2)
 button_add.grid(row=5,column=0)
 button_equal.grid(row=5,column=1,columnspan=2)
+
+button_subtract.grid(row=6,column=0)
+button_multiply.grid(row=6,column=1)
+button_divide.grid(row=6,column=2)
 
 root.mainloop()
