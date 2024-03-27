4e4e7d9e5ea39d835af63a1a3dc2df6919a920c0
Jason B <jasonbetsargon@gmail.com>
2024-03-26 18:29:25 -0700

    added status bar

--- image_viewer.py
+++ image_viewer.py
@@ -13,7 +13,8 @@ my_img5 = ImageTk.PhotoImage(Image.open("images/water.jpg"))
 
 image_list = [my_img1,my_img2,my_img3,my_img4,my_img5]
 
-
+status = tk.Label(root,text="Image 1 of " + str(len(image_list)), 
+	bd=1,relief=tk.SUNKEN,anchor=tk.E)
 
 my_label = tk.Label(image=my_img1)
 my_label.grid(row=0,column=0,columnspan=3)
@@ -35,6 +36,10 @@ def forward(image_number):
 	button_back.grid(row=1,column=0)
 	button_forward.grid(row=1,column=2)
 
+	status = tk.Label(root,text="Image " + str(image_number) + " of " + str(len(image_list)), 
+		bd=1,relief=tk.SUNKEN,anchor=tk.E)
+	status.grid(row=2,column=0,columnspan=3,sticky=tk.W+tk.E)
+
 def back(image_number):
 	global my_label
 	global button_forward
@@ -52,15 +57,19 @@ def back(image_number):
 	button_back.grid(row=1,column=0)
 	button_forward.grid(row=1,column=2)
 
+	status = tk.Label(root,text="Image " + str(image_number) + " of " + str(len(image_list)), 
+		bd=1,relief=tk.SUNKEN,anchor=tk.E)
+	status.grid(row=2,column=0,columnspan=3,sticky=tk.W+tk.E)
+
 button_back = tk.Button(root, text="<<", command=back, state=tk.DISABLED)
 button_exit = tk.Button(root, text="Exit Program", command = root.quit)
 button_forward = tk.Button(root, text=">>", command=lambda: forward(2))
 
 button_back.grid(row=1,column=0)
 button_exit.grid(row=1,column=1)
-button_forward.grid(row=1,column=2)
+button_forward.grid(row=1,column=2,pady=10)
 
-
+status.grid(row=2,column=0,columnspan=3,sticky=tk.W+tk.E)
 
 
 
